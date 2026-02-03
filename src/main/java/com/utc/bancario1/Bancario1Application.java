a02d5f454458d5d58855779e5b72d03c82b0fbeb
cristian7197 <cristian.roblero7197@utc.edu.ec>
2026-02-03 09:04:08 -0500

    A

--- src/main/java/com/utc/bancario1/Bancario1Application.java
+++ src/main/java/com/utc/bancario1/Bancario1Application.java
@@ -2,19 +2,19 @@ package com.utc.bancario1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Bancario1Application extends SpringBootServletInitializer {
+public class Bancario1Application extends SpringBootServletInitializer { // <--- 1. HEREDA DE ESTO
 
-    public static void main(String[] args) {
-        SpringApplication.run(Bancario1Application.class, args);
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) { // <--- 2. AGREGA ESTE MÉTODO
+        return application.sources(Bancario1Application.class);
     }
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Bancario1Application.class, args);
+	}
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        // Esto es necesario para desplegar en Tomcat externo
-        return builder.sources(Bancario1Application.class);
-    }
 }
